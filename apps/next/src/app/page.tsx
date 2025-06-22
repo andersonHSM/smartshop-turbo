dc61f3d1fdfb29c7dc34db15e2f0f30b688aa57b
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-06-22 11:36:34 -0300

    Add `ui` components and Tailwind improvements to `next` app
    - Introduced reusable `Button`, `Label`, and `Input` components using Radix UI and utility libraries for enhanced UI consistency.
    - Updated Tailwind CSS configuration with new token definitions, variants, and `tw-animate-css` integration.
    - Enhanced `page.tsx` with improved UI layout, modern form elements, and responsive styling.
    - Added utility functions (`cn`) for class composition with `clsx` and `tailwind-merge`.
    - Updated `pnpm-lock.yaml` and `package.json` with new dependencies (`@radix-ui`, `clsx`, `lucide-react`, etc.).

--- apps/next/src/app/page.tsx
+++ apps/next/src/app/page.tsx
@@ -1,9 +1,17 @@
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
     return (
         <div className='bg-amber-50 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
             <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
                 <h1 className='text-4xl font-bold text-blue-900'>Search your product</h1>
-                <input type='text' placeholder='Search...' className='w-full max-w-[600px] p-4 border border-gray-300 rounded-md' />
+                <div className='grid w-full max-w-sm items-center gap-3'>
+                    <Label htmlFor='email'>Product</Label>
+                    <Input autoCapitalize={'off'} type='text' id='search' placeholder='Product' />
+                    <Button className={'bg-blue-900'}>Submit</Button>
+                </div>
             </main>
         </div>
     );
